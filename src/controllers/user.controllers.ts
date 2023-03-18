923dd79fd034bef01760cc4744732902f4b48e90
Gift <Giftibe62@gmail.com>
2023-03-18 19:19:35 +0100

    feat: added features for user controller

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,23 +1,46 @@
 import userServices from "../services/user.services";
 import express, { Request, Response } from "express";
+const { findByEmail, createUser, deleteUser, findByusername } = new userServices
+
 
 class userControllers {
     async createUser(req: Request, res: Response) {
         let success: boolean = false;
+        const { email, username } = req.body
+
+
         try {
             const reqBody = req.body
-            //if the room type doesn't exist, we create one
-            const newUser = await userServices.createUser(req.body)
+
+            //checks if email is already in use
+            if (await findByEmail(email)) {
+                res.status(409)
+                    .send({
+                        message: 'email already exist',
+                        success: true,
+                    });
+            }
+            //checks if username already exist
+            if (await findByusername(username)) {
+                res.status(409)
+                    .send({
+                        message: 'username already exist',
+                        success: true,
+                    });
+            }
+
+            //Now creates user if above conditons are satified
+            const newUser = await createUser(req.body)
             res.status(201)
                 .send({
-                    message: 'error',
+                    message: 'User created',
                     success: true,
-
                 });
+
         }
         catch (err) {
             res.status(500)
-                .send({ message: 'error', success: false });
+                .send({ message: err, success: false });
         }
     }
 }
